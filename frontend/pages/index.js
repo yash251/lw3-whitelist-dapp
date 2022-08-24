5836269d50863c84ee7a5e284cc33c6236aff633
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-24 20:26:07 +0530

    provider,signer fxn

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Web3Modal from 'web3modal'
+import { useState, useEffect, useRef } from 'react'
+import { providers, Contract } from 'ethers'
+import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants/index'
 
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
+
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Switch to Rinkeby network");
+      throw new Error("Change network to Rinkeby");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  }
+
   return (
     <div className={styles.container}>
       <Head>
