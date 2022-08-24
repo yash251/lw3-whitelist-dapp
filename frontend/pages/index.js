e1c71f1a07e13db7cb4cfd8502198c41cfa77eaf
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-24 21:28:35 +0530

    removed Image

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Web3Modal from 'web3modal'
 import { useState, useEffect, useRef } from 'react'
